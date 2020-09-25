ac0afd8de49e9cf777de61bdcff0764dca5640c4
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-09-25 21:32:18 +0530

    Testing the CI/CD pipeline

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+
+import {
+    createMuiTheme,
+    responsiveFontSizes,
+    ThemeProvider,
+} from '@material-ui/core/styles';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 const Hello = () => {
-    return <div>Welcome to Project-Apollo</div>;
+    return (
+        <ThemeProvider theme={theme}>
+            <Typography
+                variant="h3"
+                component="h2"
+                color="textPrimary"
+                align="center"
+                gutterBottom
+            >
+                Welcome to Project-Amsterdam
+            </Typography>
+        </ThemeProvider>
+    );
 };
 
 export default Hello;
