48e48dc5a4730ded13a5022c23e0261e019752f0
Ritesh Patil <riteshsp2000@gmail.com>
2020-09-26 04:44:15 +0530

    update Test

--- src/__tests__/Hello.test.js
+++ src/__tests__/Hello.test.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
-import Hello from '../components/Hello';
+import Hello from '../views/Hello';
 
 let container = null;
 beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement('div');
-    document.body.appendChild(container);
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
 it('renders the welcome message', () => {
-    act(() => {
-        render(<Hello />, container);
-    });
-    expect(container.textContent).toBe('Welcome to Project-Apollo');
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe('Welcome to Project-Apollo');
 });
