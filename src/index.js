943473e6210473f7dedaf57f3298d811d301aca8
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-09-26 12:34:41 +0530

    Resolved a type

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Root from './Root';
+import Root from './root';
 import App from './views/App';
 
 import './index.css';
 
 ReactDOM.render(
-  <Root>
-    <App />
-  </Root>,
-  document.querySelector('#root')
+    <Root>
+        <App />
+    </Root>,
+    document.querySelector('#root')
 );
