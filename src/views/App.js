27747d282a75f49ed51e2a48771457daf516595f
Rohit Khandelwal <56815686+rohitk2001@users.noreply.github.com>
2020-11-15 15:27:35 +0530

    Updated App.js with Contact component.

--- src/views/App.js
+++ src/views/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import { Router, Route, Redirect, Switch } from "react-router-dom";
 
-import { ThemeProvider } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
 
-import createBrowserHistory from '../history';
-import Hello from './Hello';
-import Navbar from './Navbar';
+import createBrowserHistory from "../history";
+import Hello from "./Hello";
+import Navbar from "./Navbar";
+import ImageAvatars from "./Contact/Contact";
 
-import theme from '../theme';
+import theme from "../theme";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <ImageAvatars />
       <Router history={createBrowserHistory}>
         <Switch>
           <Navbar />
-          <Route path='/' exact>
+          <Route path="/" exact>
             <Hello />
           </Route>
-          <Redirect to='/' />
+          <Redirect to="/" />
         </Switch>
       </Router>
     </ThemeProvider>
