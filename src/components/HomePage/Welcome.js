05f359eab128f45434b1d1b955a4243333c6cb3a
Chandan Khatua <chandankhatua7@gmail.com>
2020-11-16 12:46:18 +0530

    welcome section is updated with a svg image and text

--- src/components/HomePage/Welcome.js
+++ src/components/HomePage/Welcome.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography } from '@material-ui/core';
 
 import SectionLayout from './SectionLayout';
+import { ReactComponent as ReactSvgImg } from '../../assets/bulb.svg';
 
 function Welcome({ onScrollClick }) {
   const classes = useStyle();
 
   return (
     <SectionLayout onScrollClick={onScrollClick}>
-      <h1 className={classes.heading}>Welcome Section</h1>
+      <Grid container>
+        {/*---the svg image (bulb)---*/}
+        <Grid
+          item
+          xs={12}
+          container
+          className={classes.svgContainer}
+          direction='row'
+          justify='center'
+          alignItems='center'
+        >
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <ReactSvgImg width='600px' height='500px' />
+          </Grid>
+        </Grid>
+
+        {/*----- Text ----- */}
+
+        <Grid
+          item
+          xs={12}
+          container
+          className={classes.textContainer}
+          direction='row'
+          justify='center'
+          alignItems='center'
+        >
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <Typography gutterBottom variant='h2'>
+              WELCOME TO
+            </Typography>
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <Typography gutterBottom variant='h1'>
+              <div className={classes.line}>OPENCODE</div>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </SectionLayout>
   );
 }
@@ -16,8 +56,32 @@ function Welcome({ onScrollClick }) {
 export default Welcome;
 
 const useStyle = makeStyles((theme) => ({
-  heading: {
+  svgContainer: {
+    position: 'absolute',
+    top: '0',
+    height: '100%',
+  },
+  textContainer: {
     fontFamily: theme.typography.fontFamily,
-    fontSize: '3em',
+    alignSelf: 'center',
+
+    '& h2': {
+      fontSize: '2.5rem',
+      lineHeight: '4.2rem',
+      letterSpacing: '1.2rem',
+      fontWeight: '700',
+      mixBlendMode: 'difference',
+    },
+
+    '& h1': {
+      color: 'black',
+      fontSize: '3rem',
+      lineHeight: '4.2rem',
+      letterSpacing: '3rem',
+      fontWeight: '700',
+      WebkitTextStroke: '1px whitesmoke',
+      textShadow: '1px 1px 0px #e3e3e3',
+      mixBlendMode: 'screen',
+    },
   },
 }));
